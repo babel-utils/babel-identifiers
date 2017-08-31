f04a5bd8a124139a9214b199b66ed6229eb6138c
James Kyle <me@thejameskyle.com>
2017-08-31 10:08:54 +1000

    Add getIdentifierGrammar and isIdentifierLike methods

--- test.js
+++ test.js
@@ -1,144 +1,183 @@
 // @flow
 'use strict';
 
+const cases = require('jest-in-case');
 const createBabylonOptions = require('babylon-options');
-const {getIdentifierKind} = require('./');
-const babel = require('babel-core');
+const {isIdentifierLike, getIdentifierKind, getIdentifierGrammar} = require('./');
+const printAST = require('ast-pretty-print');
+const babel = require('@babel/core');
 
-function run(code, plugins) {
-  let parserOpts = createBabylonOptions({
-    stage: 1,
-    plugins,
+function parse(code, plugins) {
+  let filePath;
+
+  babel.transform(code, {
+    ast: false,
+    code: false,
+    babelrc: false,
+    parserOpts: createBabylonOptions({
+      stage: 1,
+      plugins,
+    }),
+    plugins: [{
+      visitor: {
+        Program(path) {
+          filePath = path.hub.file.path;
+        }
+      },
+    }]
   });
 
-  let file = new babel.File({
-    options: { parserOpts },
-    passes: [],
-  });
+  return filePath;
+}
 
-  file.addCode(code);
-  file.parseCode(code);
+function getKindsAndGrammars(path) {
+  let kinds = [];
+  let grammars = [];
 
-  let results = [];
-
-  file.path.traverse({
-    'Identifier|TypeParameter'(path) {
-      results.push(`${path.node.name}:${getIdentifierKind(path)}`);
+  path.traverse({
+    enter(path) {
+      if (isIdentifierLike(path)) {
+        kinds.push(`${path.node.name}:${getIdentifierKind(path)}`);
+        grammars.push(`${path.node.name}:${getIdentifierGrammar(path)}`);
+      }
     }
   });
 
-  return results;
+  return {kinds, grammars};
 }
 
-// const print = (t, value) => {
-//   let printed = printAST(value) + '\n';
-//   return t.expressionStatement(
-//     t.templateLiteral([
-//       t.templateElement({ raw: printed }, true)
-//     , ])
-//   );
-// };
+cases('identifiers', opts => {
+  let path = parse(opts.code, opts.plugins);
+  let {kinds, grammars} = getKindsAndGrammars(path);
 
-const TESTS = [{
+  try {
+    expect(kinds).toEqual(opts.kinds);
+    expect(grammars).toEqual(opts.grammars);
+  } catch (err) {
+    console.log('---\n\n' + opts.code + '\n\n---\n\n' + printAST(path, true) + '\n\n---');
+    throw err;
+  }
+}, [{
   name: 'member expression',
-  plugins: ['flow'],
+  plugins: [],
   code: 'a.b',
-  result: ['a:reference', 'b:static'],
+  kinds: ['a:reference', 'b:static'],
+  grammars: ['a:javascript', 'b:javascript'],
 }, {
   name: 'member expression computed',
-  plugins: ['flow'],
+  plugins: [],
   code: 'a[b]',
-  result: ['a:reference', 'b:reference'],
+  kinds: ['a:reference', 'b:reference'],
+  grammars: ['a:javascript', 'b:javascript'],
 }, {
   name: 'object property',
-  plugins: ['flow'],
+  plugins: [],
   code: '({ a: b })',
-  result: ['a:static', 'b:reference'],
+  kinds: ['a:static', 'b:reference'],
+  grammars: ['a:javascript', 'b:javascript'],
 }, {
   name: 'object property computed',
-  plugins: ['flow'],
+  plugins: [],
   code: '({ [a]: b })',
-  result: ['a:reference', 'b:reference'],
+  kinds: ['a:reference', 'b:reference'],
+  grammars: ['a:javascript', 'b:javascript'],
 }, {
   name: 'object method',
-  plugins: ['flow'],
+  plugins: [],
   code: '({ a() {} })',
-  result: ['a:static'],
+  kinds: ['a:static'],
+  grammars: [],
+  grammars: ['a:javascript'],
 }, {
   name: 'object method computed',
-  plugins: ['flow'],
+  plugins: [],
   code: '({ [a]() {} })',
-  result: ['a:reference'],
+  kinds: ['a:reference'],
+  grammars: ['a:javascript'],
 }, {
   name: 'let object pattern',
-  plugins: ['flow'],
+  plugins: [],
   code: 'let {a} = {}',
-  result: ['a:static', 'a:binding'],
+  kinds: ['a:static', 'a:binding'],
+  grammars: ['a:javascript', 'a:javascript'],
 }, {
   name: 'let object pattern nested',
-  plugins: ['flow'],
+  plugins: [],
   code: 'let {a: {b}} = {}',
-  result: ['a:static', 'b:static', 'b:binding'],
+  kinds: ['a:static', 'b:static', 'b:binding'],
+  grammars: ['a:javascript', 'b:javascript', 'b:javascript'],
 }, {
   name: 'let object pattern renamed',
-  plugins: ['flow'],
+  plugins: [],
   code: 'let {a: b} = {}',
-  result: ['a:static', 'b:binding'],
+  kinds: ['a:static', 'b:binding'],
+  grammars: ['a:javascript', 'b:javascript'],
 }, {
   name: 'function param object pattern',
-  plugins: ['flow'],
+  plugins: [],
   code: '(({a}) => {})',
-  result: ['a:static', 'a:binding'],
+  kinds: ['a:static', 'a:binding'],
+  grammars: ['a:javascript', 'a:javascript'],
 }, {
   name: 'function param object pattern nested',
-  plugins: ['flow'],
+  plugins: [],
   code: '(({a: {b}}) => {})',
-  result: ['a:static', 'b:static', 'b:binding'],
+  kinds: ['a:static', 'b:static', 'b:binding'],
+  grammars: ['a:javascript', 'b:javascript', 'b:javascript'],
 }, {
   name: 'function param object pattern renamed',
-  plugins: ['flow'],
+  plugins: [],
   code: '(({a: b}) => {})',
-  result: ['a:static', 'b:binding'],
+  kinds: ['a:static', 'b:binding'],
+  grammars: ['a:javascript', 'b:javascript'],
 }, {
   name: 'array expression',
-  plugins: ['flow'],
+  plugins: [],
   code: '[a]',
-  result: ['a:reference'],
+  kinds: ['a:reference'],
+  grammars: ['a:javascript'],
 }, {
   name: 'array pattern',
-  plugins: ['flow'],
+  plugins: [],
   code: '[a] = []',
-  result: ['a:reference'],
+  kinds: ['a:reference'],
+  grammars: ['a:javascript'],
 }, {
   name: 'array pattern nested',
-  plugins: ['flow'],
+  plugins: [],
   code: '[[a]] = []',
-  result: ['a:reference'],
+  kinds: ['a:reference'],
+  grammars: ['a:javascript'],
 }, {
   name: 'let array pattern',
-  plugins: ['flow'],
+  plugins: [],
   code: 'let [a, ...b] = []',
-  result: ['a:binding', 'b:binding'],
+  kinds: ['a:binding', 'b:binding'],
+  grammars: ['a:javascript', 'b:javascript'],
 }, {
   name: 'let array pattern nested',
   plugins: ['flow'],
   code: 'let [[a, ...b]] = []',
-  result: ['a:binding', 'b:binding'],
+  kinds: ['a:binding', 'b:binding'],
+  grammars: ['a:javascript', 'b:javascript'],
 }, {
   name: 'let array pattern nested object',
-  plugins: ['flow'],
+  plugins: [],
   code: 'let { a: [b, ...c] } = {}',
-  result: ['a:static', 'b:binding', 'c:binding'],
+  kinds: ['a:static', 'b:binding', 'c:binding'],
+  grammars: ['a:javascript', 'b:javascript', 'c:javascript'],
 }, {
   name: 'let',
-  plugins: ['flow'],
+  plugins: [],
   code: 'let a = b',
-  result: ['a:binding', 'b:reference'],
+  kinds: ['a:binding', 'b:reference'],
+  grammars: ['a:javascript', 'b:javascript'],
 }, {
   name: 'function declaration',
-  plugins: ['flow'],
+  plugins: [],
   code: 'function a(b, ...c) {}',
-  result: ['a:binding', 'b:binding', 'c:binding'],
+  kinds: ['a:binding', 'b:binding', 'c:binding'],
+  grammars: ['a:javascript', 'b:javascript', 'c:javascript'],
 }, {
   name: 'function arg pattern',
   plugins: ['flow'],
@@ -161,214 +200,290 @@ const TESTS = [{
   result: ['a:binding', 'b:static', 'c:static', 'd:static', 'd:binding', 'e:reference'],
 }, {
   name: 'function expression',
-  plugins: ['flow'],
+  plugins: [],
   code: '(function a(b, ...c) {})',
-  result: ['a:binding', 'b:binding', 'c:binding'],
+  kinds: ['a:binding', 'b:binding', 'c:binding'],
+  grammars: ['a:javascript', 'b:javascript', 'c:javascript'],
 }, {
   name: 'class declaration',
-  plugins: ['flow'],
+  plugins: [],
   code: 'class a {}',
-  result: ['a:binding'],
+  kinds: ['a:binding'],
+  grammars: ['a:javascript'],
 }, {
   name: 'class expression',
-  plugins: ['flow'],
+  plugins: [],
   code: '(class a {})',
-  result: ['a:binding'],
+  kinds: ['a:binding'],
+  grammars: ['a:javascript'],
 }, {
   name: 'for loop',
-  plugins: ['flow'],
+  plugins: [],
   code: 'for (let a;;) {}',
-  result: ['a:binding'],
+  kinds: ['a:binding'],
+  grammars: ['a:javascript'],
 }, {
   name: 'for loop reference',
-  plugins: ['flow'],
+  plugins: [],
   code: 'for (a;;) {}',
-  result: ['a:reference'],
+  kinds: ['a:reference'],
+  grammars: ['a:javascript'],
 }, {
   name: 'import default',
-  plugins: ['flow'],
+  plugins: [],
   code: 'import a from ""',
-  result: ['a:binding'],
+  kinds: ['a:binding'],
+  grammars: ['a:javascript'],
 }, {
   name: 'import named',
-  plugins: ['flow'],
+  plugins: [],
   code: 'import {a} from ""',
-  result: ['a:binding', 'a:static'],
+  kinds: ['a:binding', 'a:static'],
+  grammars: ['a:javascript', 'a:javascript'],
 }, {
   name: 'import named renamed',
-  plugins: ['flow'],
+  plugins: [],
   code: 'import {a as b} from ""',
-  result: ['b:binding', 'a:static'],
+  kinds: ['b:binding', 'a:static'],
+  grammars: ['b:javascript', 'a:javascript'],
 }, {
   name: 'export default from',
-  plugins: ['flow'],
+  plugins: [],
   code: 'export a from ""',
-  result: ['a:static'],
+  kinds: ['a:static'],
+  grammars: ['a:javascript'],
 }, {
   name: 'export namespace from',
-  plugins: ['flow'],
+  plugins: [],
   code: 'export * as a from ""',
-  result: ['a:static'],
+  kinds: ['a:static'],
+  grammars: ['a:javascript'],
 }, {
   name: 'export named',
-  plugins: ['flow'],
+  plugins: [],
   code: 'export {a}',
-  result: ['a:reference', 'a:static'],
+  kinds: ['a:reference', 'a:static'],
+  grammars: ['a:javascript', 'a:javascript'],
 }, {
   name: 'export named renamed',
-  plugins: ['flow'],
+  plugins: [],
   code: 'export {a as b}',
-  result: ['a:reference', 'b:static'],
+  kinds: ['a:reference', 'b:static'],
+  grammars: ['a:javascript', 'b:javascript'],
 }, {
   name: 'export named from',
-  plugins: ['flow'],
+  plugins: [],
   code: 'export {a} from ""',
-  result: ['a:static', 'a:static'],
+  kinds: ['a:static', 'a:static'],
+  grammars: ['a:javascript', 'a:javascript'],
 }, {
   name: 'export named from renamed',
-  plugins: ['flow'],
+  plugins: [],
   code: 'export {a as b} from ""',
-  result: ['a:static', 'b:static'],
+  kinds: ['a:static', 'b:static'],
+  grammars: ['a:javascript', 'b:javascript'],
 }, {
   name: 'type alias',
   plugins: ['flow'],
   code: 'type a = {}',
-  result: ['a:binding'],
+  kinds: ['a:binding'],
+  grammars: ['a:flow'],
 }, {
   name: 'interface declaration',
   plugins: ['flow'],
   code: 'interface a {}',
-  result: ['a:binding'],
+  kinds: ['a:binding'],
+  grammars: ['a:flow'],
 }, {
   name: 'type params declaration',
   plugins: ['flow'],
   code: 'type a<b: c = d> = {}',
-  result: ['a:binding', 'b:binding', 'c:reference', 'd:reference'],
+  kinds: ['a:binding', 'b:binding', 'c:reference', 'd:reference'],
+  grammars: ['a:flow', 'b:flow', 'c:flow', 'd:flow'],
 }, {
   name: 'class type param',
   plugins: ['flow'],
   code: 'class a<b> {}',
-  result: ['a:binding', 'b:binding'],
+  kinds: ['a:binding', 'b:binding'],
+  grammars: ['a:javascript', 'b:flow'],
 }, {
   name: 'class extends',
-  plugins: ['flow'],
+  plugins: [],
   code: 'class a extends b {}',
-  result: ['a:binding', 'b:reference'],
+  kinds: ['a:binding', 'b:reference'],
+  grammars: ['a:javascript', 'b:javascript'],
 }, {
   name: 'class extends type param',
   plugins: ['flow'],
   code: 'class a extends b<c> {}',
-  result: ['a:binding', 'b:reference', 'c:reference'],
+  kinds: ['a:binding', 'b:reference', 'c:reference'],
+  grammars: ['a:javascript', 'b:javascript', 'c:flow'],
 }, {
   name: 'class implements',
   plugins: ['flow'],
   code: 'class a implements b {}',
-  result: ['a:binding', 'b:reference'],
+  kinds: ['a:binding', 'b:reference'],
+  grammars: ['a:javascript', 'b:flow'],
 }, {
   name: 'class implements type param',
   plugins: ['flow'],
   code: 'class a implements b<c> {}',
-  result: ['a:binding', 'b:reference', 'c:reference'],
+  kinds: ['a:binding', 'b:reference', 'c:reference'],
+  grammars: ['a:javascript', 'b:flow', 'c:flow'],
 }, {
   name: 'function type return',
   plugins: ['flow'],
   code: 'type a = () => b',
-  result: ['a:binding', 'b:reference'],
+  kinds: ['a:binding', 'b:reference'],
+  grammars: ['a:flow', 'b:flow'],
 }, {
   name: 'function type param',
   plugins: ['flow'],
   code: 'type a = (b: c) => {}',
-  result: ['a:binding', 'b:static', 'c:reference'],
+  kinds: ['a:binding', 'b:static', 'c:reference'],
+  grammars: ['a:flow', 'b:flow', 'c:flow'],
 }, {
   name: 'function type param unnamed',
   plugins: ['flow'],
   code: 'type a = (b) => {}',
-  result: ['a:binding', 'b:reference'],
+  kinds: ['a:binding', 'b:reference'],
+  grammars: ['a:flow', 'b:flow'],
 }, {
   name: 'function type type param',
   plugins: ['flow'],
   code: 'type a = <b>() => {}',
-  result: ['a:binding', 'b:binding'],
+  kinds: ['a:binding', 'b:binding'],
+  grammars: ['a:flow', 'b:flow'],
 }, {
   name: 'object type property',
   plugins: ['flow'],
   code: 'type a = { b: c }',
-  result: ['a:binding', 'b:static', 'c:reference'],
+  kinds: ['a:binding', 'b:static', 'c:reference'],
+  grammars: ['a:flow', 'b:flow', 'c:flow'],
 }, {
   name: 'object type method',
   plugins: ['flow'],
   code: 'type a = { b(): c }',
-  result: ['a:binding', 'b:static', 'c:reference'],
+  kinds: ['a:binding', 'b:static', 'c:reference'],
+  grammars: ['a:flow', 'b:flow', 'c:flow'],
 }, {
   name: 'object type indexer',
   plugins: ['flow'],
   code: 'type a = { [b: c]: d }',
-  result: ['a:binding', 'b:static', 'c:reference', 'd:reference'],
+  kinds: ['a:binding', 'b:static', 'c:reference', 'd:reference'],
+  grammars: ['a:flow', 'b:flow', 'c:flow', 'd:flow'],
 }, {
   name: 'object type computed property',
   plugins: ['flow'],
   code: 'type a = { [b]: c }',
-  result: ['a:binding', 'b:reference', 'c:reference'],
+  kinds: ['a:binding', 'b:reference', 'c:reference'],
+  grammars: ['a:flow', 'b:flow', 'c:flow'],
 }, {
   name: 'ts interface',
   plugins: ['typescript'],
   code: 'interface a {}',
-  result: ['a:binding'],
+  kinds: ['a:binding'],
+  grammars: ['a:typescript'],
 }, {
   name: 'ts enum',
   plugins: ['typescript'],
   code: 'enum a {}',
-  result: ['a:binding'],
+  kinds: ['a:binding'],
+  grammars: ['a:typescript'],
 }, {
   name: 'ts alias',
   plugins: ['typescript'],
   code: 'type a = {}',
-  result: ['a:binding'],
+  kinds: ['a:binding'],
+  grammars: ['a:typescript'],
 }, {
   name: 'ts namespace',
   plugins: ['typescript'],
   code: 'namespace a {}',
-  result: ['a:binding'],
+  kinds: ['a:binding'],
+  grammars: ['a:typescript'],
 }, {
   name: 'ts module',
   plugins: ['typescript'],
   code: 'module a {}',
-  result: ['a:binding'],
+  kinds: ['a:binding'],
+  grammars: ['a:typescript'],
 }, {
   name: 'ts type param instantiation',
   plugins: ['typescript'],
   code: 'class a<b> {}',
-  result: ['a:binding', 'b:binding'],
+  kinds: ['a:binding', 'b:binding'],
+  grammars: ['a:javascript', 'b:typescript'],
 }, {
   name: 'ts type param instantiation extends',
   plugins: ['typescript'],
   code: 'class a<b extends c> {}',
-  result: ['a:binding', 'b:binding', 'c:reference'],
+  kinds: ['a:binding', 'b:binding', 'c:reference'],
+  grammars: ['a:javascript', 'b:typescript', 'c:typescript'],
 }, {
   name: 'ts type param',
   plugins: ['typescript'],
   code: 'class a extends b<c> {}',
-  result: ['a:binding', 'b:reference', 'c:reference'],
+  kinds: ['a:binding', 'b:reference', 'c:reference'],
+  grammars: ['a:javascript', 'b:javascript', 'c:typescript'],
 }, {
   name: 'ts mapped type',
   plugins: ['typescript'],
   code: 'type a = { [b in keyof c]: d }',
-  result: ['a:binding', 'b:binding', 'c:reference', 'd:reference'],
-}];
-
-
-for (let testCase of TESTS) {
-  let testFn;
-
-  if (testCase.only) {
-    testFn = test.only;
-  } else if (testCase.skip) {
-    testFn = test.skip;
-  } else {
-    testFn = test;
-  }
-
-  testFn(testCase.name, () => {
-    expect(run(testCase.code, testCase.plugins)).toEqual(testCase.result);
-  });
-}
+  kinds: ['a:binding', 'b:binding', 'c:reference', 'd:reference'],
+  grammars: ['a:typescript', 'b:typescript', 'c:typescript', 'd:typescript'],
+}, {
+  name: 'ts type annotation',
+  plugins: ['typescript'],
+  code: 'function a(b: c) {}',
+  kinds: ['a:binding', 'b:binding', 'c:reference'],
+  grammars: ['a:javascript', 'b:javascript', 'c:typescript'],
+}, {
+  name: 'jsx element',
+  plugins: ['jsx'],
+  code: '<a/>',
+  kinds: ['a:static'],
+  grammars: ['a:jsx'],
+}, {
+  name: 'jsx element uppercase',
+  plugins: ['jsx'],
+  code: '<A/>',
+  kinds: ['A:reference'],
+  grammars: ['A:jsx'],
+}, {
+  name: 'jsx member expression',
+  plugins: ['jsx'],
+  code: '<a.b/>',
+  kinds: ['a:reference', 'b:static'],
+  grammars: ['a:jsx', 'b:jsx'],
+}, {
+  name: 'jsx element property',
+  plugins: ['jsx'],
+  code: '<a b/>',
+  kinds: ['a:static', 'b:static'],
+  grammars: ['a:jsx', 'b:jsx'],
+}, {
+  name: 'jsx element property with value',
+  plugins: ['jsx'],
+  code: '<a b={true}/>',
+  kinds: ['a:static', 'b:static'],
+  grammars: ['a:jsx', 'b:jsx'],
+}, {
+  name: 'jsx element spread property',
+  plugins: ['jsx'],
+  code: '<a {...b}/>',
+  kinds: ['a:static', 'b:reference'],
+  grammars: ['a:jsx', 'b:javascript'],
+}, {
+  name: 'jsx children',
+  plugins: ['jsx'],
+  code: '<a>{b}</a>',
+  kinds: ['a:static', 'b:reference', 'a:static'],
+  grammars: ['a:jsx', 'b:javascript', 'a:jsx'],
+}, {
+  name: 'jsx spread children',
+  plugins: ['jsx'],
+  code: '<a>{...b}</a>',
+  kinds: ['a:static', 'b:reference', 'a:static'],
+  grammars: ['a:jsx', 'b:javascript', 'a:jsx'],
+}]);
